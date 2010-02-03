de798eb85ec082e646140cdae6a028d14f0e885b
Bayo Erinle <berinle@gmail.com>
2010-02-03 12:42:04 -0500

    fixed mapping

--- src/main/java/net/berinle/s3mvc/web/controller/HelloWorldController.java
+++ src/main/java/net/berinle/s3mvc/web/controller/HelloWorldController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/")
 public class HelloWorldController {
 
 	//@RequestMapping(method=RequestMethod.GET, value="/welcome.do")
